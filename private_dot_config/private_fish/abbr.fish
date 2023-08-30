f69ca49f0b0dab05e699e95cc2ff50453e764b7f
benjacoblee <benjacoblee@gmail.com>
2023-08-30 13:17:11 +0800

    Add trash-cli & go

--- private_dot_config/private_fish/abbr.fish
+++ private_dot_config/private_fish/abbr.fish
@@ -1,11 +1,20 @@
 # misc
 # source vars & reload shell
+
 abbr c clear
 abbr h $HOME
 abbr x exit
 abbr chmox 'chmod +x'
 abbr grant 'echo $(passage show Master) | sudo -S' # if i need to do a lot of sudo stuff
 abbr shmoji 'shmoji fzf | xclip -selection clipboard'
+abbr rm '# Don\'t use this! Use `trash-cli` instead' # https://github.com/andreafrancia/trash-cli
+
+# trash
+abbr t trash
+abbr tp trash-put
+abbr te trash-empty
+abbr tl trash-list
+abbr trm trash-rm
 
 # fish
 abbr gofish 'clear && source $fishconf && exec fish'
@@ -44,3 +53,8 @@ abbr fd fdfind
 
 # dev
 abbr nrd 'npm run dev'
+
+# go
+abbr goru 'go run'
+abbr gomt 'go mod tidy'
+abbr gomi 'go mod init'
