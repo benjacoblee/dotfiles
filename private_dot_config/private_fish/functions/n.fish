0b6ae86d3a85deb1f017e48fb3401560af92ba93
benjacoblee <benjacoblee@gmail.com>
2024-11-12 03:30:37 +0800

    better

--- private_dot_config/private_fish/functions/n.fish
+++ private_dot_config/private_fish/functions/n.fish
@@ -1,10 +1,9 @@
-function ncr
+function n
   set RP (realpath $argv[-1])
   set FILE (string split "/" $RP)[-1]
   set FILENAME (string split "." $FILE)[1]
-  echo $FILE
-  echo $FILENAME
   set RP_DELETE (string replace $FILE $FILENAME $RP)
-  nim c -r $RP 
+  set CMDS $argv[1..-2]
+  nim $CMDS $RP
   trash $RP_DELETE
 end
